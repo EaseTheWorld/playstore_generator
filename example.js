ca113a24629066814d38083642a4cc29cf98005d
EaseTheWorld <easetheworld@gmail.com>
2026-05-31 00:02:00 +0900

    First commit

---
+++ example.js
@@ -0,0 +1,41 @@
+const DEFAULT_DATA = {
+  "header": {
+    "title": "안드로이드 앱 포트폴리오",
+    "mainTag": "✓ 이 기기",
+    "extraTags": [
+      "오래된 순 ▾",
+      "에디터 추천"
+    ]
+  },
+  "body": [
+    {
+      "title": "Coupang",
+      "desc": "Korean Best Shopping App",
+      "iconUrl": "https://play-lh.googleusercontent.com/X5-X2S0t7G9dTGrPftk-5hXijqRDhwWKxGDs2gBm_kNPcAlO3re4exC_8nekvDhz-H0=w240-h480-rw",
+      "tag": [
+        "4.7 ★",
+        "5,000만 이상"
+      ],
+      "url": "https://play.google.com/store/apps/details?id=com.coupang.mobile"
+    },
+    {
+      "title": "Naver",
+      "desc": "Korean Best Portal",
+      "iconUrl": "https://play-lh.googleusercontent.com/X5-X2S0t7G9dTGrPftk-5hXijqRDhwWKxGDs2gBm_kNPcAlO3re4exC_8nekvDhz-H0=s64-rw",
+      "tag": [
+        "4.2 ★",
+        "1억 이상"
+      ]
+    },
+    {
+      "title": "AliExpress",
+      "desc": "Alibaba Mobile • 쇼핑",
+      "iconUrl": "https://play-lh.googleusercontent.com/z-N5NXRU3S-TqK-E-Eb-I6G1D-n-tSpyM-b-X-o-l-bK-D-m-t-q-B-E-b-S-w=w240-h480-rw",
+      "tag": [
+        "<HTML>",
+        "Special Chars"
+      ],
+      "url": "https://play.google.com/store/apps/details?id=com.alibaba.aliexpresshd"
+    }
+  ]
+};
